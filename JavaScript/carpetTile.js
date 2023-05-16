0177b450af14c70b5af56b4d4065f0eac41dd924
Galondor <johnyjoejoe55@gmail.com>
2023-05-16 12:28:34 -0400

    CptTile Page

---
+++ JavaScript/carpetTile.js
@@ -0,0 +1,48 @@
+const products = document.getElementById('products');
+const product = document.getElementById('product');
+const productName = document.querySelector('.product_name');
+const productPrice = document.querySelector('.product_price');
+const productImg = document.getElementById('product_img');
+
+getProducts();
+
+async function getProducts() {
+  products.innerHTML = "";
+  localStorage.removeItem('selectedProduct');
+
+  try {
+    const response = await fetch('/productData/products.json');
+    const data = await response.json();
+    const productData = data.filter(product => product.productType === "Carpet Tile");
+    console.log(productData);
+
+      for (let i = 0; i < 3; i++) {
+        products.innerHTML += `<a href="/itemPage.html" class="product_wrapper cptTile_wrapper" onclick="selectedProduct(${productData[i].sfnStyleNumber})" >
+        <div class="product" id="product" style="background-image: url(${productData[i].image})">
+        <div class="product_banner">
+            <h3 class="product_name">${productData[i].sfnName}</h3>
+            <h4 class="product_brand">${productData[i].brand}</h4>
+            <div class="desc_wrapper">
+                <p class="price">${productData[i].price[0].priceValue}<span class="uom">/sqft</span></p>
+                <p class="rating">${productData[i].rating}</p>
+            </div>
+            <p class="total_colors">${productData[i].colors.length} Color Options Available</p>
+            <div class="colors">
+            <figure class="color_wrapper"><img src="${productData[i].colors[0].colorImage}" alt=""></figure>
+            <figure class="color_wrapper"><img src="${productData[i].colors[1].colorImage}" alt=""></figure>
+            <figure class="color_wrapper"><img src="${productData[i].colors[2].colorImage}" alt=""></figure>
+            <figure class="color_wrapper"><img src="${productData[i].colors[3].colorImage}" alt=""></figure>
+        </div>
+        </div>
+    </div></a>`;
+    console.log(i)
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function selectedProduct(product) {
+  //Get the poduct data from the clicked product
+  localStorage.setItem('selectedProduct', product);
+}
