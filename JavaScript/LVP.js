0f1062a2b04f8e808bcf95f5e07145d0b9e11909
Galondor <johnyjoejoe55@gmail.com>
2023-06-13 17:40:15 -0400

    Responsiveness Pt2

--- JavaScript/LVP.js
+++ JavaScript/LVP.js
@@ -16,9 +16,10 @@ async function getProducts() {
     const productData = data.filter(product => product.productType === "LVP");
     console.log(productData);
 
+    if (window.innerWidth > 425) {
       for (let i = 0; i < productData.length; i++) {
         products.innerHTML += `<a href="itemPage.html" class="product_wrapper" onclick="selectedProduct(${productData[i].sfnStyleNumber})" >
-        <div class="product" id="product" style="background-image: url(../${productData[i].image})" loading="lazy">
+        <div class="product" id="product" value="${productData[i].rating}" style="background-image: url(../${productData[i].image})" loading="lazy" type="jpeg">
         <div class="product_banner">
             <h3 class="product_name">${productData[i].sfnName}</h3>
             <h4 class="product_brand">${productData[i].brand}</h4>
@@ -28,14 +29,34 @@ async function getProducts() {
             </div>
             <p class="total_colors">${productData[i].colors.length} Color Options Available</p>
             <div class="colors">
-            <figure class="color_wrapper"><img src="../${productData[i].colors[0].colorImage}" alt="" loading="lazy"></figure>
-            <figure class="color_wrapper"><img src="../${productData[i].colors[1].colorImage}" alt="" loading="lazy"></figure>
-            <figure class="color_wrapper"><img src="../${productData[i].colors[2].colorImage}" alt="" loading="lazy"></figure>
-            <figure class="color_wrapper"><img src="../${productData[i].colors[3].colorImage}" alt="" loading="lazy"></figure>
+            <figure class="color_wrapper"><img src="../${productData[i].colors[0].colorImage}" alt="" loading="lazy" type="jpeg"></figure>
+            <figure class="color_wrapper"><img src="../${productData[i].colors[1].colorImage}" alt="" loading="lazy" type="jpeg"></figure>
+            <figure class="color_wrapper"><img src="../${productData[i].colors[2].colorImage}" alt="" loading="lazy" type="jpeg"></figure>
+            <figure class="color_wrapper"><img src="../${productData[i].colors[3].colorImage}" alt="" loading="lazy" type="jpeg"></figure>
         </div>
         </div>
     </div></a>`;
-    console.log(i)
+    }
+    } else if (window.innerWidth <= 425) {
+      for (let i = 0; i < productData.length; i++) {
+        products.innerHTML += `<a href="itemPage.html" class="product_wrapper" onclick="selectedProduct(${productData[i].sfnStyleNumber})" >
+        <div class="product" id="product" value="${productData[i].rating}" style="background-image: url(../${productData[i].image})" loading="lazy" type="jpeg">
+        <div class="product_banner">
+            <h3 class="product_name">${productData[i].sfnName}</h3>
+            <h4 class="product_brand">${productData[i].brand}</h4>
+            <div class="desc_wrapper">
+                <p class="price">${productData[i].price[0].priceValue}<span class="uom">/sqft</span></p>
+                <p class="rating">${productData[i].rating}</p>
+            </div>
+            <p class="total_colors">${productData[i].colors.length} Color Options Available</p>
+            <div class="colors">
+            <figure class="color_wrapper"><img src="../${productData[i].colors[0].colorImage}" alt="" loading="lazy" type="jpeg"></figure>
+            <figure class="color_wrapper"><img src="../${productData[i].colors[1].colorImage}" alt="" loading="lazy" type="jpeg"></figure>
+            <figure class="color_wrapper"><img src="../${productData[i].colors[2].colorImage}" alt="" loading="lazy" type="jpeg"></figure>
+        </div>
+        </div>
+    </div></a>`;
+    }
     }
   } catch (error) {
     console.log(error);
--- JavaScript/global.js
+++ JavaScript/global.js
@@ -46,6 +46,7 @@ navBar.innerHTML = `
             </div>
         </div>
     </div>
+    <svg class="mobile_menu_btn" onclick="openMenu()" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
 </div>`;}
 
 const footer = document.getElementById("footer");
@@ -159,5 +160,55 @@ function selectedProduct(product) {
     }
   
     localStorage.setItem("recentlyViewed", JSON.stringify(recent));
+  }
+
+  function openMenu() {
+    const cart = JSON.parse(localStorage.getItem("cart")) || [];
+    const menu = document.querySelector(".mobile_menu");
+    menu.style.display = "flex";
+    menu.innerHTML = `
+            <div class="menu_overlay"></div>
+            <svg class="close_menu_btn" onclick="closeMenu()" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
+            <div class="menu_background">
+            <div class="menu_item" onclick="closeMenu()">
+                <a href="quiz.html">Flooring Quiz</a>
+            </div>
+            <div class="menu_item" onclick="closeMenu()">
+                <a href="bookings.html">Estimate</a>
+            </div>
+            <div class="menu_item" onclick="closeMenu()">
+                <a href="locations.html">Contact</a>
+            </div>
+            <div class="menu_item" onclick="closeMenu()">
+                <a href="homepage.html">Home</a>
+            </div>
+            <div class="menu_item" onclick="closeMenu()">
+                <a href="cart.html">View Cart <span class="mobile_cart_amt" >(${cart.length})<span></a>
+            </div>
+            <div class="menu_divider"></div>
+            <div class="menu-item store_item" onclick="closeMenu()">
+                <a href="store.html">All Products</a>
+            </div>
+            <div class="menu-item store_item" onclick="closeMenu()">
+                <a href="carpet.html">Carpet</a>
+            </div>
+            <div class="menu-item store_item" onclick="closeMenu()">
+                <a href="LVP.html">Luxury Vinyl Plank</a>
+            </div>
+            <div class="menu-item store_item" onclick="closeMenu()">
+                <a href="hardwood.html">Hardwood</a>
+            </div>
+            <div class="menu-item store_item" onclick="closeMenu()">
+                <a href="carpetTile.html">Carpet Tile</a>
+            </div></div>`
+            const closeMenu = document.querySelector(".close_menu_btn");
+            closeMenu.style.display = "flex";
+  }
+
+  function closeMenu() {
+    const menu = document.querySelector(".mobile_menu");
+    const closeMenu = document.querySelector(".close_menu_btn");
+    closeMenu.style.display = "none";
+    menu.style.display = "none";
   }
   
